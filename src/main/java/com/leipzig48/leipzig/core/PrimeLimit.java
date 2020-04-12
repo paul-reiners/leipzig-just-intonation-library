7d33042e9931604404b519c7c6501b6be519b3a0
Paul Reiners <paul.reiners@gmail.com>
2020-04-12 12:59:34 -0500

    Compiles.

---
+++ src/main/java/com/leipzig48/leipzig/core/PrimeLimit.java
@@ -0,0 +1,56 @@
+/*
+ * Created on Dec 4, 2004
+ *
+ * Leipzig: A Just Intonation Library
+ * Copyright (C) 2004 Paul Reiners
+ *
+ * This library is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation; either version 2.1 of the License, or (at your option) any later
+ * version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with this library; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * Contact Info:
+ *
+ * 	Paul Reiners
+ * 	2506 18 1/2 Ave NW
+ * 	Apt 206
+ * 	Rochester, MN  55901
+ *
+ * 	paulreiners@earthlink.net
+ */
+package com.leipzig48.leipzig.core;
+
+/**
+ * @author Paul Reiners
+ */
+class PrimeLimit {
+
+    final static PrimeLimit FIVE_LIMIT = new PrimeLimit(5);
+
+    private int limit;
+
+    PrimeLimit(int limit) {
+        // TODO Throw exception:
+//		assert Utilities.isPrime(limit) : "Limit must be a prime.";
+
+        this.limit = limit;
+    }
+
+    boolean limits(long n) {
+        for (int i = limit + 1; i <= n; i++) {
+            if (Utilities.isPrime(i) && n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
