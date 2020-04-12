f98f3dec2e277c8f75a986279ef473264e358cef
Paul Reiners <paul.reiners@gmail.com>
2020-04-12 18:05:00 -0500

    Converted to JPanel.

--- src/main/java/com/leipzig48/leipzig/samples/JustIntonationKeyboard.java
+++ src/main/java/com/leipzig48/leipzig/samples/JustIntonationKeyboard.java
@@ -35,11 +35,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
-import javax.swing.ButtonGroup;
-import javax.swing.JApplet;
-import javax.swing.JCheckBox;
-import javax.swing.JPanel;
-import javax.swing.JRadioButton;
+import javax.swing.*;
 
 import com.leipzig48.leipzig.core.FiveLimitChord;
 import com.leipzig48.leipzig.core.FiveLimitTransposition;
@@ -54,7 +50,7 @@ import com.leipzig48.leipzig.lattices.FiveLimitLattice;
  * 
  * Based on Nick Didkovsky's VirtualKeyboard
  */
-public class JustIntonationKeyboard extends JApplet implements ItemListener,
+public class JustIntonationKeyboard extends JPanel implements ItemListener,
 		ActionListener {
 
 	/**
@@ -127,7 +123,7 @@ public class JustIntonationKeyboard extends JApplet implements ItemListener,
 		return synthNotePanel;
 	}
 
-	public void start() {
+	public JustIntonationKeyboard() {
 		// TODO Fix.
 //		JMSL.setIsApplet(true);
 //		JSynMusicDevice.instance().open();
@@ -141,7 +137,7 @@ public class JustIntonationKeyboard extends JApplet implements ItemListener,
 		rootY = 0;
 		buildTuning();
 		mainPanel = new JPanel();
-		getContentPane().add(mainPanel);
+		this.add(mainPanel);
 		buildVirtualKeys();
 		buildChordButtons();
 		buildTranspositionPanel();
@@ -150,6 +146,39 @@ public class JustIntonationKeyboard extends JApplet implements ItemListener,
 
 		JPanel synthNotePanel = buildSynthNotePanel();
 		mainPanel.add(synthNotePanel);
+	}
+
+
+	/**
+	 * Create the GUI and show it. For thread safety, this method should be
+	 * invoked from the event-dispatching thread.
+	 */
+	private static void createAndShowGUI() {
+		//Make sure we have nice window decorations.
+		JFrame.setDefaultLookAndFeelDecorated(true);
+
+		//Create and set up the window.
+		JFrame frame = new JFrame("Chord Selection");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		//Create and set up the content pane.
+		JComponent newContentPane = new JustIntonationKeyboard();
+		newContentPane.setOpaque(true); //content panes must be opaque
+		frame.setContentPane(newContentPane);
+
+		//Display the window.
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		//Schedule a job for the event-dispatching thread:
+		//creating and showing this application's GUI.
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
 	}
 
 	/**
